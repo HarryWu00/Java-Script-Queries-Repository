813bce4718a0bd997a6ec252d3a3f954e8b6c064
HarryWu00 <8hwu@stpatrick.org>
2017-03-22 11:27:19 -0500

    Day 2

---
+++ click.js
@@ -0,0 +1,11 @@
+//declare samecolor() here
+function samecolor(hex){
+  let samecolor = document.getElemsntsByTagName("samecolor")
+  for (let i=0;i<samecolor;i++){
+    samecolor[i].style.color = newcoler;
+  }
+}
+//declare diffcolor() here
+function diffcolor(hex){
+
+}
